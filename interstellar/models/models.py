d939449f85e24a4ef16e10d4be97a02029193579
Ivan Tortosa Gimeno <ivantorgim@gmail.com>
2023-10-30 11:20:38 +0100

    finish basic models

--- interstellar/models/models.py
+++ interstellar/models/models.py
@@ -8,6 +8,13 @@ class player(models.Model):
     _description = 'Jugador de Interstellar'
 
     name = fields.Char(string='Nombre')
+    birthdate = fields.Date(string='Fecha de nacimiento')
+
+    planets = fields.One2many(
+        string='Planetas',
+        comodel_name='interstellar.planet',
+        inverse_name='player'
+    )
 
 
 class planet(models.Model):
@@ -20,9 +27,14 @@ class planet(models.Model):
     race = fields.Char(string='Raza')
     age = fields.Integer(string='Edad', )
 
-    precious_minerals = fields.Integer(default=100, string='Minerales preciosos')
+    minerals = fields.Integer(default=100, string='Minerales')
     energy = fields.Integer(default=500, string='Energía')
     construction_materials = fields.Integer(default=500, string='Materiales de construcción')
+
+    player = fields.Many2one(
+        string='Jugador',
+        comodel_name='interstellar.player'
+    )
 
     spaceships = fields.Many2many(
         string='Naves',
@@ -38,14 +50,23 @@ class spaceship(models.Model):
     _description = 'Nave'
 
     name = fields.Char(string='Nombre')
+    description = fields.Text(string='Descripción')
+    attack = fields.Integer(string='Ataque')
+    health = fields.Integer(string='salud')
+    mineral_cost = fields.Integer(string='Coste en minerales')
+    material_cost = fields.Integer(string='Coste en materiales')
+    mineral_collection = fields.Integer(string='Recolección de minerales')
+    material_collection = fields.Integer(string='Recolección de materiales')
 
     planets = fields.Many2many(
+        string='Planetas',
         comodel_name='interstellar.planet',
         relation='planet_spaceship_rel',
         column1='spaceship_id',
         column2='planet_id'
     )
     weapons = fields.Many2many(
+        string='Armas',
         comodel_name='interstellar.weapon',
         relation='spaceship_weapon_rel',
         column1='spaceship_id',
@@ -58,7 +79,12 @@ class weapon(models.Model):
     _description = 'Arma'
 
     name = fields.Char(string='Nombre')
+    description = fields.Text(string='Descripción')
+    attack = fields.Integer(string='Ataque')
+    reload = fields.Integer(string='Recarga')
+
     spaceships = fields.Many2many(
+        string='Naves',
         comodel_name='interstellar.spaceship',
         relation='spaceship_weapon_rel',
         column1='weapon_id',
